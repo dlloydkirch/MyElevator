72e36774621f69d7a76c849edc05ad475d5fb61b
David Kirch <dlloydkirch@gmail.com>
2019-05-15 19:50:13 -0500

    initial commit

---
+++ MyElevator/IElevator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyElevator
+{
+    public interface IElevator<T>
+    {
+        void changeFloors();
+        void doorControls();
+        void buttonPanelControl();
+        void emergencyBrakes();
+
+    }
+}
---
+++ MyElevator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyElevator
+{
+    public class Elevator : IElevator<Elevator>
+    {
+
+        public void changeFloors()
+        {
+        }
+
+        public void doorControls()
+        {
+        }
+
+        public void emergencyBrakes()
+        {
+            bool emergencyBrake = true;
+            if (emergencyBrake)
+            {
+                bool continueMoving = false;
+                bool callAuthorities = true;
+                if (callAuthorities)
+                {
+                    initiateResponse();
+                }
+            }
+        }
+
+        public void buttonPanelControl()
+        {
+
+        }
+        public void initiateResponse() { }
+
+    }
+}
